8fa07b0aabb14c0af3c386147af7131b46edfdf4
Lívia Keller <101295392+kellerlivia@users.noreply.github.com>
2022-10-20 15:42:45 -0300

    Views and Comments

--- InstaClone/SettingsViewController.swift
+++ InstaClone/SettingsViewController.swift
@@ -6,17 +6,21 @@
 //
 
 import UIKit
+import Firebase
 
 class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-
-    @IBAction func logoutButton(_ sender: UIButton) {
-        performSegue(withIdentifier: "toVC", sender: nil)
+    
+    @IBAction func logoutClicked(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "toViewController", sender: nil)
+        } catch {
+            print("error")
+        }
     }
-
 }
--- InstaClone/ViewController.swift
+++ InstaClone/ViewController.swift
@@ -10,41 +10,82 @@ import Firebase
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
     
+    // o que carrega
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func signButton(_ sender: UIButton) {
         
-        performSegue(withIdentifier: "toFeedVC", sender: nil)
     }
     
-    @IBAction func signUpButton(_ sender: UIButton) {
+    @IBAction func signInClicked(_ sender: UIButton) {
+        authUser()
+    }
+    
+    
+    @IBAction func signUpClicked(_ sender: UIButton) {
+        createUser()
+    }
+    
+    // funcao para criar alerta que recebe um titulo e uma mensagem
+    func makeAlert(titleInput: String, messageInput: String) {
+        // constante para armazenar o alerta
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        // botao para sair do alerta
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        // colocando a acao do botao no alerta
+        alert.addAction(okButton)
+        // voltando para a tela
+        self.present(alert, animated: true, completion: nil)
+    }
+}
+
+//MARK: - Auth
+
+extension ViewController {
+    
+    func authUser () {
         
-        if emailTextField.text != "" && passwordTextField.text != "" {
-            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { authdata, error in
+        // caso os atributos email e senha sejam diferentes de nulo
+        if emailText.text != "" && passwordText.text != "" {
+            // fazendo a autenticacao pelo que o usuario digita nos campos
+            Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!) { (authdata, error) in
+                // se o erro for diferente de nulo
                 if error != nil {
-                    makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                    // cria um alerta com o titulo "Error!" e a mensagem do erro
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                } else {
+                    // indo para a tela do feed
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        } else {
+            // criando o alerta para o usuario digitar os campos em vazio
+            makeAlert(titleInput: "Error!", messageInput: "Username/Password?")
+        }
+    }
+}
+
+//MARK: - Create
+
+extension ViewController {
+    
+    func createUser () {
+        
+        // caso os atributos email e senha sejam diferentes de nulo
+        if emailText.text != "" && passwordText.text != "" {
+            // criando o usuario
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { (authdata, error) in
+                
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
                 } else {
                     self.performSegue(withIdentifier: "toFeedVC", sender: nil)
                 }
             }
         } else {
-            makeAlert(titleInput: "Error!", messageInput: "Username/password?")
-        }
-        
-        func makeAlert(titleInput: String, messageInput: String) {
-            
-            let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
-            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
-            alert.addAction(okButton)
-            self.present(alert, animated: true, completion: nil)
-
+            makeAlert(titleInput: "Error!", messageInput: "Username/Password?")
         }
     }
 }
-
